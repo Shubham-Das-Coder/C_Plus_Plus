7a8c9543785471526eda626f6e22d0e78027150a
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-10-25 20:27:12 +0530

    Create Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    cout << "Please Enter The First Number:";
+    cin >> num1;
+    int num2;
+    cout << "Please Enter The Second Number:";
+    cin >> num2;
+    int sum = num1 + num2;
+    cout << num1 << " + " << num2 << " = " << sum;
+    return 0;
+}
