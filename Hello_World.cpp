393657bac5abd1235ccdd336c14ddbb0d33f1ccd
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-10-25 20:20:33 +0530

    Create Hello_World.cpp

---
+++ Hello_World.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/*
+#include preprocessor directive used to include files
+iostream is the header file for taking input and printing output
+*/
+int main()
+/*
+The execution of the code begins from main function which is mandatory for every program
+*/
+{
+    std::cout << "Hello World\n"; // std is namespace standard
+    /*
+    cout is used to display the output in double quotes
+    \n adds line break
+    ; marks the end of the statement
+    */
+    return 0; // Exit status of a function
+}
