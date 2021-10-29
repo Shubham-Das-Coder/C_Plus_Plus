e06c5fb8c06ae21ccb12e77c1ff5e28c4b610c92
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-10-29 17:21:42 +0530

    Create ASCII_Value.cpp

---
+++ ASCII_Value.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout << "Please Enter A Character : ";
+    cin >> c;
+    cout << "ASCII Value Of " << c << " Is " << int(c);
+    return 0;
+}
